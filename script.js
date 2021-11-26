1a381abd1bd3175e21644478a5c85024ab6175e1
mseidel819 <mseidel819@gmail.com>
2021-11-26 13:04:17 -0600

    response improvements

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ navLinks.addEventListener('click', function (e) {
 //menu fade
 
 //scroll slector for projects
+
+//about selector buttons
+const tabsContainer = document.querySelector('.about__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
