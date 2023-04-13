a56afafe0cdd016d2ea9bc27595752381583e63a
Matt Seidel <mseidel819@gmail.com>
2023-04-13 18:53:58 -0500

    all section draft

--- src/project-data.js
+++ src/project-data.js
@@ -1,67 +1,76 @@
 const project1 = {
-  img: 'img/pet_adopter.webp',
-  title: 'Dog Finder',
+  img: '',
+  title: 'Tech2Live',
   description:
-    'The Dog-Finder app accesses the Petfinder API in order to search for adoptable dogs within 10 miles of an entered location. After selecting a dog, its info/details, as well as the organization caring for it, are rendered dynamically. This project also uses MVC architecture.',
-  goal: "The goal of this project was to create a page from scratch based off of the skills learned from completing both 'The Complete JavaScript Course 2022: From Zero to Expert!', and 'Build Responsive Real-World Websites with HTML and CSS' on Udemy.com.",
-  tech: 'HTML, CSS, JavaScript, Parcel.js',
-  demo: 'https://startling-mochi-d0adad.netlify.app/',
-  gitHub: 'https://github.com/mseidel819/pet_adopter',
+    'Tech2Live is an early stage volunteer startup in the Tech-for-Good space. Our goal is to create a platform to financially aid survivors of domestic and sexual violence through the support of donors.',
+  tech: ['HTML', 'CSS', 'Sass', 'JavaScript'],
+  demo: 'https://tech2live.netlify.app/',
 };
 
 const project2 = {
-  img: 'img/portfolio.webp',
-  title: 'My Portfolio',
-  description:
-    'This is a page designed from the ground-up to introduce myself and show my projects to potential employers.',
-  goal: 'The educational goal for this page was to build a complete page that was mobile-respoinsive, used best practices, was aesthetically pleasing, and had a Lighthouse score above 95.',
-  tech: 'HTML, CSS, JavaScript. The particle effect was shared by Vincent Garreau',
-  demo: 'https://seidelmatt.com/',
-  gitHub: 'https://github.com/mseidel819/mseidel819.github.io',
-};
-
-const project3 = {
-  img: 'img/githubScreenshot.webp',
-  title: 'GitHub Search App',
-  description:
-    "This is a solution to the GitHub user search app challenge on Frontend Mentor. Users should be able to view the optimal layout for the app depending on their device's screen size, see hover states for all interactive elements on the page, search for GitHub users by their username, see relevant user information based on their search, and switch between light and dark themes",
-  goal: 'To create a functional web app that accesses the GitHub API from strict design parameters using React and Material UI.',
-  tech: 'JavaScript, React, Material UI, Emotion, API',
-  demo: 'https://chimerical-frangipane-1137e5.netlify.app/',
-  gitHub: 'https://github.com/mseidel819/github-user-search-app',
-};
-
-const project4 = {
-  img: 'img/job-listing-ss.webp',
-  title: 'Static Job Listings',
-  description:
-    "This is a solution to the Job listings with filtering challenge on Frontend Mentor. Users should be able to view the optimal layout for the site depending on their device's screen size, see hover states for all interactive elements on the page, and filter job listings based on the categories",
-  goal: 'To create a functional web app from strict design parameters using React and Material UI.',
-  tech: 'JavaScript, React, Material UI, Emotion',
-  demo: 'https://astonishing-bavarois-d35327.netlify.app/',
-  gitHub: 'https://github.com/mseidel819/static-job-listings',
-};
-
-const project5 = {
-  img: 'img/calc-react.webp',
-  title: ' Calculator w/ Redux and themes',
-  description:
-    "This is a solution to the calculator app challenge on Frontend Mentor. Figma files were provided. Users should be able to view the optimal layout for the app depending on their device's screen size, see hover states for all interactive elements on the page, perform mathmatical operations like addition, subtraction, multiplication, and division, and adjust the color theme based on their preference",
-  goal: 'To create a functional web app from strict design parameters using React, Redux, and Material UI.',
-  tech: 'JavaScript, React, Redux, Material UI, Emotion for styling',
-  demo: 'https://statuesque-pastelito-391c96.netlify.app/',
-  gitHub: 'https://github.com/mseidel819/calculator-redux',
-};
-
-const project6 = {
   img: 'img/comments-redux.webp',
   title: 'Comments Section w/ Redux',
   description:
     "This is a solution to the interactive comments section challenge on Frontend Mentor. Figma files were provided. Users should be able to view the optimal layout for the app depending on their device's screen size, see hover states for all interactive elements on the page, Create, Read, Update, and Delete comments and replies, Upvote and downvote comments, and use localStorage to save the current state in the browser that persists when the browser is refreshed.",
   goal: 'To create a functional web app from strict design parameters using React, Material UI, and Redux.',
-  tech: 'Material Ui, JavaScript, React, Redux, React-Redux, Redux-Persist, Redux-Logger, and Reselect',
+  tech: ['Material Ui', 'JavaScript', 'React', 'Redux'],
   demo: 'https://tranquil-capybara-a9a135.netlify.app/',
   gitHub: 'https://github.com/mseidel819/comments-section-redux',
+};
+
+const project3 = {
+  img: 'img/pet_adopter.webp',
+  title: 'Dog Finder',
+  description:
+    'The Dog-Finder app accesses the Petfinder API in order to search for adoptable dogs within 10 miles of an entered location. After selecting a dog, its info/details, as well as the organization caring for it, are rendered dynamically. This project also uses MVC architecture.',
+  goal: "The goal of this project was to create a page from scratch based off of the skills learned from completing both 'The Complete JavaScript Course 2022: From Zero to Expert!', and 'Build Responsive Real-World Websites with HTML and CSS' on Udemy.com.",
+  tech: ['HTML', 'CSS', 'JavaScript'],
+  demo: 'https://startling-mochi-d0adad.netlify.app/',
+  gitHub: 'https://github.com/mseidel819/pet_adopter',
+};
+
+// const project2 = {
+//   img: 'img/portfolio.webp',
+//   title: 'My Portfolio',
+//   description:
+//     'This is a page designed from the ground-up to introduce myself and show my projects to potential employers.',
+//   goal: 'The educational goal for this page was to build a complete page that was mobile-respoinsive, used best practices, was aesthetically pleasing, and had a Lighthouse score above 95.',
+//   tech: 'HTML, CSS, JavaScript. ',
+//   demo: 'https://seidelmatt.com/',
+//   gitHub: 'https://github.com/mseidel819/mseidel819.github.io',
+// };
+
+const project4 = {
+  img: 'img/githubScreenshot.webp',
+  title: 'GitHub Search App',
+  description:
+    "This is a solution to the GitHub user search app challenge on Frontend Mentor. Users should be able to view the optimal layout for the app depending on their device's screen size, see hover states for all interactive elements on the page, search for GitHub users by their username, see relevant user information based on their search, and switch between light and dark themes",
+  goal: 'To create a functional web app that accesses the GitHub API from strict design parameters using React and Material UI.',
+  tech: ['JavaScript', 'React', 'Material UI', 'Emotion', 'RestAPI'],
+  demo: 'https://chimerical-frangipane-1137e5.netlify.app/',
+  gitHub: 'https://github.com/mseidel819/github-user-search-app',
+};
+
+const project5 = {
+  img: 'img/job-listing-ss.webp',
+  title: 'Static Job Listings',
+  description:
+    "This is a solution to the Job listings with filtering challenge on Frontend Mentor. Users should be able to view the optimal layout for the site depending on their device's screen size, see hover states for all interactive elements on the page, and filter job listings based on the categories",
+  goal: 'To create a functional web app from strict design parameters using React and Material UI.',
+  tech: ['JavaScript', 'React', 'Material UI', 'Emotion'],
+  demo: 'https://astonishing-bavarois-d35327.netlify.app/',
+  gitHub: 'https://github.com/mseidel819/static-job-listings',
+};
+
+const project6 = {
+  img: 'img/calc-react.webp',
+  title: ' Calculator w/ Redux and themes',
+  description:
+    "This is a solution to the calculator app challenge on Frontend Mentor. Figma files were provided. Users should be able to view the optimal layout for the app depending on their device's screen size, see hover states for all interactive elements on the page, perform mathmatical operations like addition, subtraction, multiplication, and division, and adjust the color theme based on their preference",
+  goal: 'To create a functional web app from strict design parameters using React, Redux, and Material UI.',
+  tech: ['JavaScript', 'React', 'Redux', 'Material UI', 'Emotion'],
+  demo: 'https://statuesque-pastelito-391c96.netlify.app/',
+  gitHub: 'https://github.com/mseidel819/calculator-redux',
 };
 
 export const projectData = [
@@ -71,4 +80,24 @@ export const projectData = [
   project4,
   project5,
   project6,
+];
+
+export const skillsArr = [
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Svelte',
+  'Material-UI',
+  'Node.js',
+  'Express',
+  'MongoDB',
+];
+export const softSkillsArr = [
+  'Problem Solving',
+  'Continuing Education',
+  'Teamwork',
+  'Empathy',
+  'Patience',
+  'Communication',
 ];
--- src/script.js
+++ src/script.js
@@ -1,2 +1,42 @@
 'use strict';
-import { projectData } from './project-data';
+import { projectData, skillsArr, softSkillsArr } from './project-data';
+
+const techSkillContainer = document.querySelector('.tech-skills-container');
+const softSkillContainer = document.querySelector('.soft-skills-container');
+
+skillsArr.forEach(skill => {
+  techSkillContainer.insertAdjacentHTML(
+    'beforeend',
+    `
+<p class="header-2 tech-skill-item">${skill.toUpperCase()}</p>
+`
+  );
+});
+
+softSkillsArr.forEach(skill => {
+  softSkillContainer.insertAdjacentHTML(
+    'beforeend',
+    `
+  <p class="header-2 soft-skill-item">${skill.toUpperCase()}</p>
+  `
+  );
+});
+
+const projectsContainer = document.querySelector('.projects-container');
+
+projectData.forEach(project => {
+  projectsContainer.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="project-clump">
+        <img class="project-img" src="" alt=""/>
+        <h3 class="header-3">${project.title}</h3>
+        <div class="tech-container">
+            ${project.tech
+              .map(item => `<p class="body-1 tech-item">${item}</p>`)
+              .join('')}
+        </div>
+    </div>
+    `
+  );
+});
